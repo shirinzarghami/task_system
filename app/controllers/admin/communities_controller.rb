e7ebace4407d091757f5637cf15d17bc8fb5d7da
Martijn Bolhuis <martijn.bolhuis@gmail.com>
2012-07-17 16:25:00 +0200

    * Continued work with admin/community

--- app/controllers/admin/communities_controller.rb
+++ app/controllers/admin/communities_controller.rb
@@ -4,4 +4,16 @@ class Admin::CommunitiesController < AdminController
   def index
     @communities = Community.paginate page: params[:page], per_page: 25
   end
+
+  def new
+    @community = Community.new
+  end
+
+  def create
+    
+  end
+
+  def destroy
+    
+  end
 end
--- app/models/community.rb
+++ app/models/community.rb
@@ -1,3 +1,4 @@
 class Community < ActiveRecord::Base
   attr_accessible :name, :subdomain
+  has_and_belongs_to_many :users
 end
