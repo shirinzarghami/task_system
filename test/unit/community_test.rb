f6ee5bba609cf4c55c88e0c0543007fdb18a06b1
Martijn Bolhuis <martijn.bolhuis@gmail.com>
2012-07-28 20:10:07 +0200

    * Started with a little community unit test

--- test/unit/community_test.rb
+++ test/unit/community_test.rb
@@ -4,4 +4,9 @@ class CommunityTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Community should not be valid without a subdomain" do
+    c = Community.new name: 'test community', max_users: 20
+    assert !c.save
+  end
 end
