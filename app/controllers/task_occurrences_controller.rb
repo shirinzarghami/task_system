14a5f8716a3ec91da8038fcb9c8fd0b1319360d4
Martijn Bolhuis <martijn.bolhuis@gmail.com>
2012-11-23 23:24:16 +0100

    Started with task occurrence manual instantiate dialog

--- app/controllers/task_occurrences_controller.rb
+++ app/controllers/task_occurrences_controller.rb
@@ -2,4 +2,8 @@ class TaskOccurrencesController < ApplicationController
   def index
     @my_todos = TaskOccurrence.for_user(@user).for_community(@community)
   end
+
+  def create
+    
+  end
 end
