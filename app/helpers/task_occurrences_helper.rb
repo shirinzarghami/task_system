876469302f0c72184bf2f9907f82bb77f12340a3
Martijn Bolhuis <martijn.bolhuis@gmail.com>
2012-12-29 14:38:10 +0100

    Task occurrences index reimplemented with bootstrap pills and keeping separate controller actions

--- app/helpers/task_occurrences_helper.rb
+++ app/helpers/task_occurrences_helper.rb
@@ -1,3 +1,9 @@
 module TaskOccurrencesHelper
 
+  def action_tab text, action_name
+    li_class = controller.action_name == action_name.to_s ? 'active' : ''
+    content_tag :li, class: li_class do
+      link_to text, controller: :task_occurrences, action: action_name
+    end
+  end
 end
