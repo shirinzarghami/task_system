cf4fad9c8043f7f5a21a95093af06f90df3a076d
Martijn Bolhuis <martijn.bolhuis@gmail.com>
2013-06-23 22:57:41 +0200

    Improve payment form number parsing and max deviation calculation

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -2,7 +2,7 @@ module PaymentsHelper
   def form_url
     if controller.action_name == 'update' || controller.action_name == 'edit'
       community_payment_path(@community, @payment.id)
-    elsif controller.action_name == 'new'
+    elsif controller.action_name == 'new' || controller.action_name == 'create'
       community_payments_path(@community)
     end
   end
