1ca824458fdecd6de5389fd3d0b67f8ffe297bac
Martijn Bolhuis <martijn.bolhuis@gmail.com>
2013-06-23 18:19:11 +0200

    Fixed payment test

--- test/unit/payment_test.rb
+++ test/unit/payment_test.rb
@@ -6,26 +6,29 @@ class PaymentTest < ActiveSupport::TestCase
     @payer = @community.community_users.first
   end
   context "product declaration" do
-    # should "save when sum of user modifications is 0" do
-    #   debugger  
-    #   @payment = FactoryGirl.create :product_declaration, price: 120
-    #   @community = @payment.community_user.community # 6 users in community
-    #   @community.community_users.each do |cu| 
-    #     @payment.user_saldo_modifications << FactoryGirl.create :user_saldo_modification, price: 20, community_user: cu
-    #   end
-
-    # end
-
     should "save when sum of user modifications is 0" do
+      @payment = FactoryGirl.create(:product_declaration_with_saldos, 
+        price: 100, 
+        community_user: @payer, 
+        saldo_price_1: 75,
+        community_users_set_1: [@community.community_users.first],
+        saldo_price_2: -25,
+        community_users_set_2: @community.community_users.last(3)
+        )
+      assert @payment.reload && @payment.save      
+    end
+
+    should "not save when sum is not 0" do
       @payment = FactoryGirl.create(:product_declaration_with_saldos, 
         price: 100, 
         community_user: @payer, 
         saldo_price_1: 100,
         community_users_set_1: [@community.community_users.first],
-        saldo_price_2: 25,
-        community_users_set_1: [@community.community_users.last(4)]
+        saldo_price_2: -25,
+        community_users_set_2: @community.community_users.last(3)
         )
-      
+      assert @payment.reload && !@payment.save    
+
     end
   end
 end
