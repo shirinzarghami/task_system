42330121e8f11953f2503ae433d77054bc3575ff
Martijn Bolhuis <martijn.bolhuis@gmail.com>
2013-07-21 15:56:06 +0200

    Improve schedule icons

--- app/helpers/task_occurrences_helper.rb
+++ app/helpers/task_occurrences_helper.rb
@@ -1,10 +1,13 @@
 module TaskOccurrencesHelper
 
-  def action_tab text, action_name
+  def action_tab text, action_name, options = {}
     li_class = controller.action_name == action_name.to_s ? 'active' : ''
-    content_tag :li, class: li_class do
-      link_to text, controller: :schedule, action: action_name
-    end
+
+    icon_html = options[:icon_file].present? ? content_tag(:i, image_tag(options[:icon_file])) : ''
+
+    link_html = link_to icon_html + text, controller: :schedule, action: action_name
+    content_tag :li, (link_html),class: li_class
+      
   end
 
   def number_of_comments task_occurrence
