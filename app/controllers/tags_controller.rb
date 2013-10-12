71585b0f373e89899e0518c6a887350f72e04234
Martijn Bolhuis <martijn.bolhuis@gmail.com>
2013-10-12 19:57:32 +0200

    - Improve payments icon - Tag search: show existing tags first

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,7 +4,7 @@ class TagsController < ApplicationController
   def index
     @tags = ActsAsTaggableOn::Tagging.joins(:tag).where("name LIKE '%#{params[:q]}%'").where(taggable_type: @taggable_type, tagger_type: 'Community', tagger_id: @community.id).group('tags.name').select('name as name, name as id')
     respond_to do |format|
-      format.json {render json: ([{name: params[:q], id: params[:q]}] + @tags ).to_json}
+      format.json {render json: (@tags + [{name: params[:q], id: params[:q]}] ).to_json}
     end
   end
 
