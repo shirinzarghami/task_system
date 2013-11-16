9b4402acd61e94172de554ce3332d38be0402c37
Martijn Bolhuis <martijn.bolhuis@gmail.com>
2013-11-16 10:15:53 +0100

    Clean up helpers

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,9 +1,4 @@
 module TasksHelper
-
-  # def can_edit_task? task=@task
-  #   task.user == @user or @community_user.role == 'admin'
-  # end
-
   def next_allocated_user task
     user = task.next_allocated_user
     user ? user.name : t('activerecord.attributes.task.allocation_modes_short.voluntary')
@@ -27,11 +22,8 @@ module TasksHelper
   end
 
   def task_distribution task
-    # result = TaskOccurrence.joins("RIGHT OUTER JOIN users ON task_occurrences.user_id = users.id").where(["task_occurrences.task_id = ?", task.id]).group("users.id").select('users.name as label, COUNT(users.id) as value')
     result = TaskOccurrence.joins("RIGHT OUTER JOIN users ON task_occurrences.user_id = users.id").where(["task_occurrences.task_id = ?", task.id]).group("users.id").select('users.name as label, FORMAT(SUM(task_occurrences.time_in_minutes),0) as value') 
     result = TaskOccurrence.joins("RIGHT OUTER JOIN users ON task_occurrences.user_id = users.id").where(["task_occurrences.task_id = ?", task.id]).group("users.id").select('users.name as label, SUM(task_occurrences.time_in_minutes) as value') 
-    # result.to_json
     result.map {|n| {value: n.value.to_i, label: n.label}}.to_json
-
   end
 end
