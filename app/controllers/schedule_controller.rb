94e7fa25fb1b6ecef97b8198d0133d331bd2ca08
Martijn Bolhuis <martijn.bolhuis@gmail.com>
2014-01-16 21:22:49 +0100

    Sort  completed tasks on completed_atr

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -5,21 +5,22 @@ class ScheduleController < ApplicationController
   before_filter :set_breadcrumbs
 
   include Sortable::Controller
-  sort :task_occurrence, default_column: :deadline, default_direction: :asc
+  # sort :task_occurrence, default_column: :deadline, default_direction: :desc
 
   def todo
-    # sort :task_occurrence, default_column: :deadline, default_direction: :desc
+    sort :task_occurrence, default_column: :deadline, default_direction: :desc
     add_crumb t('breadcrumbs.todos'), community_schedule_todo_path(@community)
     @task_occurrences = @community.task_occurrences.for_user_or_open(@user).todo.order(sort_column + ' ' + sort_direction).paginate(page: params[:page],per_page: 20)
   end
 
   def open
-    # sort :task_occurrence, default_column: :deadline, default_direction: :desc
+    sort :task_occurrence, default_column: :deadline, default_direction: :desc
     add_crumb t('breadcrumbs.open'), community_schedule_todo_path(@community)
     @task_occurrences = @community.task_occurrences.todo.order(sort_column + ' ' + sort_direction).paginate(page: params[:page],per_page: 20)
   end
 
   def completed
+    sort :task_occurrence, default_column: :completed_at, default_direction: :desc
     add_crumb t('breadcrumbs.completed'), community_schedule_todo_path(@community)
     @task_occurrences = @community.task_occurrences.completed.order(sort_column + ' ' + sort_direction).paginate(page: params[:page], per_page: 20)
   end
